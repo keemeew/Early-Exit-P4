0405060d9f038803f197777279c7cf4ceab3829e
keemeew <harry0475@naver.com>
2023-09-08 00:47:59 -0700

    final

--- run_adaptive.sh
+++ run_adaptive.sh
@@ -9,5 +9,5 @@ CLI_PATH=$BMV2_PATH/targets/simple_switch/simple_switch_CLI
 
 sudo PYTHONPATH=$PYTHONPATH:$BMV2_PATH/mininet/ python3 /home/p4/Early-Exit-P4/Topology.py \
    --behavioral-exe $BMV2_PATH/targets/simple_switch/simple_switch \
-   --l2switch /home/p4/Early-Exit-P4/p4src/ee_adaptive.json \
+   --l2switch ~/Early-Exit-P4/p4src/ee_adaptive.json \
    --cli $CLI_PATH
